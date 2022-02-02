3ecd022f3541d7551d9931d61044fc83282a5964
hstha <shresthaheriz15@gmail.com>
2022-02-02 21:52:06 +0545

    Introducing k8s  - created different deployment of services with their clustering ip address configuration  - created node port service for posts (still trying to figure out why)  - created ingress configuration for mapping routing

--- query/index.js
+++ query/index.js
@@ -47,7 +47,7 @@ app.listen(4002, async () => {
   console.log("Listening on 4002");
   try {
     // get events after the server starts
-    const res = await axios.get("http://localhost:4005/events");
+    const res = await axios.get("http://event-bus-cluster-ip-srv:4005/events");
 
     for (let event of res.data) {
       console.log("Processing event:", event.type);
